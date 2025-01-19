f6fc46b300d62237fd7beb66d47a8d0870d4ca97
Veronika-dev4 <semeradova.veronika@gmail.com>
2025-01-19 22:54:38 +0100

    Add files via upload

---
+++ projekt.py
@@ -0,0 +1,96 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Veronika Semeradova
+email: semeradova.veronika@gmail.com
+"""
+user = ("bob", "ann", "mike", "liz")
+password = ("123", "pass123", "password123", "pass123")
+uzivatel = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
+user_1 = input("user: ")
+password_1 = input("password: ")
+print("-" * 40)
+if uzivatel.get(user_1) == password_1:
+    print("Welcome to the app,", user_1, "\nWe have 3 texts to be analyzed.")
+    print("-" * 40)
+else:
+    print("Unregistered user, terminating the program...")
+    exit()
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+text_1 = input("Enter a number btw. 1 and 3 to select: ")
+print("-" * 40)
+if not(text_1.isnumeric()):
+    print("Wrong character.")
+    exit()
+elif int(text_1)>3:
+    print("Wrong number.")
+    exit()
+split_text = TEXTS[int(text_1)-1].split()
+number = 0
+words = 0
+upper = 0
+allupper = 0
+alllower = 0
+suma_all = 0
+for idx in range(len(split_text)):
+    if split_text[idx].isnumeric():
+        number = number + 1
+        suma_all = suma_all + int(split_text[idx])
+    elif split_text[idx][0].isupper():
+        upper = upper + 1
+    if split_text[idx].isupper():
+        allupper = allupper + 1
+    if split_text[idx].islower:
+        alllower = alllower + 1
+words = len(split_text)
+print("There are", words, "words in the selected text.")
+print("There are", upper, "titlecase words.")
+print("There are", allupper, "uppercase words.")
+print("There are", alllower, "lowercase words")
+print("There are", number, "numeric strings.")
+print("The sum of all the numbers", suma_all,".")
+print("-" * 40)
+print("LEN|", "   ", "OCCURENCES", "   ", "|NR.")
+print("-" * 40)
+dictionary_words = split_text
+length_words = {word: len(word) for word in dictionary_words}
+new = set(length_words.values())
+dict = {}
+for val in new: 
+    dict[val] = 0
+for word in split_text:
+    dict[len(word)] +=1
+print(length_words)
+print(dict)
+for item in dict:
+    print(f"{item:2} |", end="")
+    for i in range(dict[item]):
+      print("*", end="")
+    print(" ", dict[item])
+
